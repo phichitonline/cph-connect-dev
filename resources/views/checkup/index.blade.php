5c07d7bba493c6f5e3b8dca47447084a1fbd6dd3
Dr.GHOST Developer <phichitonline@gmail.com>
2022-03-30 22:03:27 +0700

    ตรวจสุขภาพ

--- resources/views/checkup/index.blade.php
+++ resources/views/checkup/index.blade.php
@@ -14,7 +14,13 @@
             <div class="flex-grow-1">
                 <div>
                     <h1 class="font-700 mb-1">{{ $moduletitle }}</h1>
+                    <p class="mb-0 pb-1 pr-3">
+                        {{ $ptname }}
+                    </p>
                 </div>
+            </div>
+            <div>
+                <img src="{{ $pic }}" data-src="{{ $pic }}" width="50" class="rounded-circle mt- shadow-xl preload-img">
             </div>
         </div>
     </div>
