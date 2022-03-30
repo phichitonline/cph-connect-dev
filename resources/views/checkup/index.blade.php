5e5a3ee9cb7dcc76aa7c2344b8e9c1e099898e06
Dr.GHOST Developer <phichitonline@gmail.com>
2022-03-30 22:07:35 +0700

    ตรวจสุขภาพ

--- resources/views/checkup/index.blade.php
+++ resources/views/checkup/index.blade.php
@@ -14,18 +14,12 @@
             <div class="flex-grow-1">
                 <div>
                     <h1 class="font-700 mb-1">{{ $moduletitle }}</h1>
-                    <p class="mb-0 pb-1 pr-3">
-
-                    </p>
                 </div>
-            </div>
-            <div>
-                
             </div>
         </div>
     </div>
 
-    <a href="{{ url('/') }}/bookcalendar/?flag=C" class="col-6 pr-0">
+    <a href="{{ url('/') }}/bookcalendar/?flag=C">
         <div class="card card-style">
             <img class="img-fluid" src="images/book_healthy.png">
         </div>
