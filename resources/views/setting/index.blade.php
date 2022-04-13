59de0ade60b8c3b9ec2f0f71ea4e8cc112141cc1
Dr.GHOST <phichitonline@gmail.com>
2022-04-13 14:58:52 +0700

    Update setting add GPS location

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -55,15 +55,14 @@
                 <input class="form-control" type="url" name="hos_youtube" value="{{ $data->hos_youtube }}">
             </div>
             <div class="input-style input-style-2 input-required">
-                <a target="_blank" href="{{ $data->hoslocation }}">
                 <span class="color-highlight input-style-1-active">พิกัด GPS ที่ตั้งโรงพยาบาล</span>
-                </a>
                 <input class="form-control" type="text" name="hoslocation" id="locationPoint" value="{{ $data->hoslocation }}">
+                <a target="_blank" href="https://www.google.co.th/maps/@{{ $data->hoslocation }}">คลิกดูตัวอย่างแผนที่</a>
                 <a href="#" onclick="getLocation()" class="btn btn-m btn-center-l font-900 bg-gray2-dark rounded-sm shadow-xl mt-2 mb-2">คลิกอ่านพิกัด GPS</a>
             </div>
         </div>
         <div class="divider mb-0"></div>
-        
+
         <div class="cal-schedule">
             <em>
                 <div class="fac fac-radio fac-green"><span></span>
