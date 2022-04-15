88a236586372609a568e3d09b4897316b12f700e
Dr.GHOST <phichitonline@gmail.com>
2022-04-15 16:24:35 +0700

    ระบบนัด Appointment

--- resources/views/appointment/cc.blade.php
+++ resources/views/appointment/cc.blade.php
@@ -34,7 +34,7 @@
 					<input type="hidden" name="que_date" value="{{ $que_date }}" readonly  />
 					<input type="hidden" name="hn" value="{{ $_SESSION['hn'] }}" readonly  />
 					<input type="hidden" name="lid" value="{{ $_SESSION['lineid'] }}" readonly  />
-					<input type="hidden" name="pname" value="{{ $ptname }}" readonly  />
+					<input type="hidden" name="ptname" value="{{ $ptname }}" readonly  />
                     <input type="hidden" name="que_dep" value="{{ $qdep }}" readonly  />
                     <input type="hidden" name="que_app_flag" value="{{ $qflag }}" readonly  />
                     <input type="hidden" name="module_name" value="{{ $module_name }}" readonly  />
