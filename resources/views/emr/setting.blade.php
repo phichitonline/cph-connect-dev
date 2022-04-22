1f2cc3509683d744ba2b02132dd51cdacd4bab89
Dr.GHOST <phichitonline@gmail.com>
2022-04-22 10:18:33 +0700

    ปรับระบบ Setting EMR

--- resources/views/emr/setting.blade.php
+++ resources/views/emr/setting.blade.php
@@ -70,14 +70,41 @@
                 <input class="form-control" type="number" name="emr_bmi2" value="{{ $settingemr->emr_bmi2 }}">
             </div>
 
-
         </div>
 
+        <div class="cal-schedule">
+            <strong>ตั้งค่า LAB specimen</strong>
+            <div class="content mb-0">
+                <div class="input-style input-style-2 input-required mt-4">
+                    <span class="color-highlight input-style-1-active">Specimen เลือด</span>
+                    <input class="form-control" type="text" name="lab_spec_blood" value="{{ $data->lab_spec_blood }}">
+                </div>
+                <div class="input-style input-style-2 input-required mt-4">
+                    <span class="color-highlight input-style-1-active">Specimen ปัสสาวะ</span>
+                    <input class="form-control" type="text" name="lab_spec_urine" value="{{ $data->lab_spec_urine }}">
+                </div>
+            </div>
+        </div>
 
         <button type="submit" class="btn btn-m btn-center-l text-uppercase font-900 bg-blue2-dark rounded-sm shadow-xl mt-4 mb-0">บันทึก</button>
         <div class="clear"><br></div>
 
     </div>
+
+    <div class="footer card card-style">
+        <a class="footer-title"><span class="color-highlight">คำแนะนำ</span></a>
+        <p class="footer-text">
+        <span class="font-12">
+            <br>ดูคำแนะนำการตั้งค่าได้ที่นี่ครับ --><a target="_blank" href="https://drive.google.com/drive/folders/1QXJuYPB84ae705tz5QmkZ7hK0dJY2DbZ?usp=sharing">คลิกที่นี่</a>
+            <br>
+        </span>
+        <span class="font-16">
+            <br><br><b>หากมีปัญหา ข้อสงสัย ต้องการคำแนะนำ ปรึกษาผู้พัฒนาได้ครับ
+                <br>Line ID = <a href="https://line.me/ti/p/Xu3TXschDY">0619921666</a>
+        </span>
+        </p>
+    </div>
+
 </div>
 
 </form>
