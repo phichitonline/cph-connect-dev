b8af3a09d59a0caeab9868854e8b986b33755a18
Dr.GHOST <phichitonline@gmail.com>
2022-11-02 12:18:04 +0700

    Book calendar

--- resources/views/book/calendar.blade.php
+++ resources/views/book/calendar.blade.php
@@ -5,8 +5,8 @@
 @endsection
 
 @section('content')
-                
-    <div class="header header-fixed header-logo-center bg-yellow1-dark">
+
+    <div class="header header-fixed header-logo-center {{ $module_color }}">
         <a href="#" onclick="goBack()" class="header-title color-white">ย้อนกลับ</a>
         <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-bell"></i></a>
@@ -35,7 +35,7 @@
                 $database_dbnurse = config('database.connections.mysql.database');
                 $username_dbnurse = config('database.connections.mysql.username');
                 $password_dbnurse = config('database.connections.mysql.password');
-                $dbnurse = mysqli_connect($hostname_dbnurse, $username_dbnurse, $password_dbnurse) or trigger_error(mysqli_error(),E_USER_ERROR); 
+                $dbnurse = mysqli_connect($hostname_dbnurse, $username_dbnurse, $password_dbnurse) or trigger_error(mysqli_error(),E_USER_ERROR);
                 mysqli_select_db($dbnurse,$database_dbnurse);
                 mysqli_set_charset($dbnurse,"utf8");
                 date_default_timezone_set("Asia/Bangkok");
@@ -44,14 +44,14 @@
                 $monthNames = array("มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม");
                 if (!isset($_REQUEST["month"])) $_REQUEST["month"] = date("n");
                 if (!isset($_REQUEST["year"])) $_REQUEST["year"] = date("Y");
-    
+
                 $cMonth = $_REQUEST["month"];
                 $cYear = $_REQUEST["year"];
                 $prev_year = $cYear;
                 $next_year = $cYear;
                 $prev_month = $cMonth - 1;
                 $next_month = $cMonth + 1;
-    
+
                 if ($prev_month == 0) {
                     $prev_month = 12;
                     $prev_year = $cYear - 1;
@@ -86,17 +86,17 @@
                 {{-- </div> --}}
 
                     @php
-    
+
                     $reserve = 5;
                     $dtime = 2;
                     $timestamp = mktime(0, 0, 0, $cMonth, 1, $cYear);
                     $maxday = date("t", $timestamp);
-    
+
                     $thismonth = getdate($timestamp);
                     $startday = $thismonth['wday'];
-    
+
                     $recdate = $cYear . '-0' . $cMonth;
-    
+
                     for ($i = 0; $i < ($maxday + $startday); $i++) {
                         if (strlen($_REQUEST["month"]) == 1) {
                             $nMonth = '0' . $_REQUEST["month"];
@@ -111,7 +111,7 @@
                         } else {
                             $ni = '-' . $ni;
                         }
-    
+
                         $chkbook = $chkdate . $ni;
                         $day = ($i % 7);
                         if (($i % 7) == 0) {
@@ -126,7 +126,7 @@
                             $row_holiday = mysqli_fetch_assoc($chkholiday);
                             if ($row_holiday['total_holiday'] > 0) $is_holiday = true;
                             else $is_holiday = false;
-    
+
                             if(date($chkbook) == date('Y-m-d', time()))
                                 if ((date('N', strtotime($chkbook)) >= 6) or $is_holiday) {
                                     echo "<a href='#' class='cal-selected color-highlight'>".$d."<p class='mb-0 mt-n3 font-10'><mark class='highlight pl-2 font-10 pr-2 bg-red2-dark'>วันนี้</mark></p></a>";
@@ -150,7 +150,7 @@
                                     } else {
                                         $total_que = "<p class='mb-0 mt-n3'><mark class='highlight pl-2 font-10 pr-2 bg-green1-dark'>".$row_chkque['total_que']." ราย</mark></p>";
                                     }
-                                    
+
                                 }
                                 echo "<a href='/booktime/?qflag=".$qflag."&flag=".$flag."&day=".$day."&que_date=".$chkbook."' data-ajax='false'><b>".$d."</b>".$total_que."</a>";
                             }
@@ -166,7 +166,7 @@
         <div class="decoration decoration-margins"></div>
 
     </div>
-    <!-- End of Page Content--> 
+    <!-- End of Page Content-->
 
 @endsection
 
